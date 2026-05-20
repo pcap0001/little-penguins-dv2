4dbf196b8b4c2534ba3bfc30799b16483980acbc
pcap0001 <pcap0001@student.monash.edu>
2026-05-20 12:36:02 +1000

    Add files via upload

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,40 @@
+var vg_1 = "js/records_by_month.vg.json";
+var vg_2 = "js/records_by_year.vg.json";
+var vg_3 = "js/records_by_penguin.vg.json";
+var vg_4 = "js/penguin_tracking_map.vg.json";
+var vg_5 = "js/penguin_tracks_map.vg.json";
+var vg_6 = "js/fledglings_over_time.vg.json";
+var vg_7 = "js/breeding_stages_by_year.vg.json";
+var vg_8 = "js/breeding_success_heatmap.vg.json";
+var vg_9 = "js/sst_vs_fledglings.vg.json";
+var vg_10 = "js/breeding_season_timing.vg.json";
+
+vegaEmbed("#records_by_month", vg_1).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#records_by_year", vg_2).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#records_by_penguin", vg_3).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#penguin_tracking_map", vg_4).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#penguin_tracks_map", vg_5).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#fledglings_over_time", vg_6).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#breeding_stages_by_year", vg_7).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#breeding_success_heatmap", vg_8).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#sst_vs_fledglings", vg_9).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#breeding_season_timing", vg_10).then(function(result) {
+}).catch(console.error);
