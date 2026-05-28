a571231bc0a42412aa7192401dd7b637579a845c
pcap0001 <pcap0001@student.monash.edu>
2026-05-28 13:50:54 +1000

    Add files via upload

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -7,6 +7,7 @@ var vg_7 = "js/breeding_stages_by_year.vg.json";
 var vg_8 = "js/breeding_success_heatmap.vg.json";
 var vg_9 = "js/sst_vs_fledglings.vg.json";
 var vg_10 = "js/breeding_season_timing.vg.json";
+var vg_11 = "js/weather_vs_fledglings.vg.json";
 
 vegaEmbed("#records_by_time", vg_1).then(function(result) {
 }).catch(console.error);
@@ -79,16 +80,206 @@ vegaEmbed("#penguin_tracks_map", vg_5).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#fledglings_over_time", vg_6).then(function(result) {
+  var view = result.view;
+
+  var colonyCheckboxIds = [
+    "showAntechamberBay",
+    "showBrownlow",
+    "showEmuBay",
+    "showGraniteIsland",
+    "showKingscote",
+    "showNepeanPines",
+    "showPenneshaw",
+    "showTroubridge",
+    "showVivonneBay",
+    "showWestIsland"
+  ];
+
+  var selectAll = document.getElementById("selectAllColonies");
+
+  function updateColonySignal(id) {
+    view.signal(id, document.getElementById(id).checked);
+  }
+
+  function updateSelectAllState() {
+    var allChecked  = colonyCheckboxIds.every(function(id) { return document.getElementById(id).checked; });
+    var noneChecked = colonyCheckboxIds.every(function(id) { return !document.getElementById(id).checked; });
+    selectAll.checked       = allChecked;
+    selectAll.indeterminate = !allChecked && !noneChecked;
+  }
+
+  colonyCheckboxIds.forEach(function(id) {
+    document.getElementById(id).addEventListener("change", function() {
+      updateColonySignal(id);
+      updateSelectAllState();
+      view.runAsync();
+    });
+  });
+
+  selectAll.addEventListener("change", function() {
+    colonyCheckboxIds.forEach(function(id) {
+      document.getElementById(id).checked = selectAll.checked;
+      updateColonySignal(id);
+    });
+    selectAll.indeterminate = false;
+    view.runAsync();
+  });
+
 }).catch(console.error);
 
 vegaEmbed("#breeding_stages_by_year", vg_7).then(function(result) {
+  var view = result.view;
+
+  var stageCheckboxIds = [
+    "showEggs",
+    "showNestlings",
+    "showFledglings"
+  ];
+
+  var selectAll = document.getElementById("selectAllStages");
+
+  function updateStageSignal(id) {
+    view.signal(id, document.getElementById(id).checked);
+  }
+
+  function updateSelectAllState() {
+    var allChecked = stageCheckboxIds.every(function(id) {
+      return document.getElementById(id).checked;
+    });
+
+    var noneChecked = stageCheckboxIds.every(function(id) {
+      return !document.getElementById(id).checked;
+    });
+
+    selectAll.checked = allChecked;
+    selectAll.indeterminate = !allChecked && !noneChecked;
+  }
+
+  stageCheckboxIds.forEach(function(id) {
+    document.getElementById(id).addEventListener("change", function() {
+      updateStageSignal(id);
+      updateSelectAllState();
+      view.runAsync();
+    });
+  });
+
+  selectAll.addEventListener("change", function() {
+    stageCheckboxIds.forEach(function(id) {
+      document.getElementById(id).checked = selectAll.checked;
+      updateStageSignal(id);
+    });
+
+    selectAll.indeterminate = false;
+    view.runAsync();
+  });
+
 }).catch(console.error);
 
 vegaEmbed("#breeding_success_heatmap", vg_8).then(function(result) {
+  var view = result.view;
+
+  var heatColonyCheckboxIds = [
+    "heatAntechamberBay",
+    "heatBrownlow",
+    "heatEmuBay",
+    "heatGraniteIsland",
+    "heatKingscote",
+    "heatNepeanPines",
+    "heatPenneshaw",
+    "heatTroubridge",
+    "heatVivonneBay",
+    "heatWestIsland"
+  ];
+
+  var selectAll = document.getElementById("selectAllHeatColonies");
+
+  function updateHeatColonySignal(id) {
+    view.signal(id, document.getElementById(id).checked);
+  }
+
+  function updateSelectAllState() {
+    var allChecked = heatColonyCheckboxIds.every(function(id) {
+      return document.getElementById(id).checked;
+    });
+
+    var noneChecked = heatColonyCheckboxIds.every(function(id) {
+      return !document.getElementById(id).checked;
+    });
+
+    selectAll.checked = allChecked;
+    selectAll.indeterminate = !allChecked && !noneChecked;
+  }
+
+  heatColonyCheckboxIds.forEach(function(id) {
+    document.getElementById(id).addEventListener("change", function() {
+      updateHeatColonySignal(id);
+      updateSelectAllState();
+      view.runAsync();
+    });
+  });
+
+  selectAll.addEventListener("change", function() {
+    heatColonyCheckboxIds.forEach(function(id) {
+      document.getElementById(id).checked = selectAll.checked;
+      updateHeatColonySignal(id);
+    });
+
+    selectAll.indeterminate = false;
+    view.runAsync();
+  });
+
 }).catch(console.error);
 
 vegaEmbed("#sst_vs_fledglings", vg_9).then(function(result) {
+  var view = result.view;
+
+  var sstCheckboxIds = [
+    "sstShowAntechamberBay",
+    "sstShowBrownlow",
+    "sstShowEmuBay",
+    "sstShowGraniteIsland",
+    "sstShowKingscote",
+    "sstShowNepeanPines",
+    "sstShowPenneshaw",
+    "sstShowTroubridge",
+    "sstShowVivonneBay",
+    "sstShowWestIsland"
+  ];
+
+  var selectAll = document.getElementById("selectAllSSTColonies");
+
+  function updateSSTSignal(id) {
+    view.signal(id, document.getElementById(id).checked);
+  }
+
+  function updateSelectAllState() {
+    var allChecked  = sstCheckboxIds.every(function(id) { return document.getElementById(id).checked; });
+    var noneChecked = sstCheckboxIds.every(function(id) { return !document.getElementById(id).checked; });
+    selectAll.checked       = allChecked;
+    selectAll.indeterminate = !allChecked && !noneChecked;
+  }
+
+  sstCheckboxIds.forEach(function(id) {
+    document.getElementById(id).addEventListener("change", function() {
+      updateSSTSignal(id);
+      updateSelectAllState();
+      view.runAsync();
+    });
+  });
+
+  selectAll.addEventListener("change", function() {
+    sstCheckboxIds.forEach(function(id) {
+      document.getElementById(id).checked = selectAll.checked;
+      updateSSTSignal(id);
+    });
+    selectAll.indeterminate = false;
+    view.runAsync();
+  });
+
 }).catch(console.error);
 
 vegaEmbed("#breeding_season_timing", vg_10).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#weather_vs_fledglings", vg_11).then(function(result) {
 }).catch(console.error);
